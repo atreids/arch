427b82146aff3c223a88d329a9468a6020221929
Aaron Donaldson <dev@adonaldson.com>
2025-12-24 12:53:13 +0000

    Update laptop install script

--- scripts/arch-install-laptop.sh
+++ scripts/arch-install-laptop.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -e
 # Install Arch Linux on Laptop
+
+disk=/dev/nvme0n1
+
+loadkeys uk
+timedatectl set-ntp true
+wipefs -af $disk
+sgdisk --zap-all $disk
+sgdisk -n 1:0:+550M -c efi -t ef00 $disk
+sgdisk -n 2:0:+4G -c swap -t 8200 $disk
+sgdisk -n 3:0:0 -c root -t 8300 $disk
